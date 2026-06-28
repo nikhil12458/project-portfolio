4253b832bd1bb94c7fd812bdf8cc447c93e92f30
Nikhil <ektak144@gmail.com>
2026-06-29 00:03:08 +0530

    Add InfiniteCarousel and CarouselCard components with GSAP animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import TextReveal from "@/components/TextReveal";
+import InfiniteCarousel from "@/components/InfiniteCarousel";
+import { projects } from "@/data/projects";
 
 export default function Home() {
   return (
-    <main className="h-[300vh] w-full bg-[#010101]">
-      <div className="h-[50%]"></div>
-      <TextReveal splitBy="chars" trigger="scroll" className="text[5rem] text-white">
-        Hello Everyone
-      </TextReveal>
+    <main className="h-screen w-full flex items-start ">
+      <InfiniteCarousel projects={projects} />
     </main>
   );
 }
