25ea52f39910f0248bab0f83a496b6e56367b841
Nikhil <ektak144@gmail.com>
2026-06-28 15:12:33 +0530

    Add TextReveal component and integrate GSAP animations with Tailwind CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import TextReveal from "@/components/TextReveal";
+
 export default function Home() {
   return (
-    <main className="h-[300vh] w-full">
-      <h1>Hello everyone</h1>
+    <main className="h-[300vh] w-full bg-[#010101]">
+      <div className="h-[50%]"></div>
+      <TextReveal splitBy="chars" trigger="scroll" className="text[5rem] text-white">
+        Hello Everyone
+      </TextReveal>
     </main>
   );
 }
